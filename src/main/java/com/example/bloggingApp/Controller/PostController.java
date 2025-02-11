f87884b9cfa01bd3d1c9b82e0e0aaaf9c24af2bf
marty957 <fantuzzimartina@yahoo.it>
2025-02-11 16:47:15 +0100

    third commit

--- src/main/java/com/example/bloggingApp/Controller/PostController.java
+++ src/main/java/com/example/bloggingApp/Controller/PostController.java
@@ -1,12 +1,41 @@
 package com.example.bloggingApp.Controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/blogPosts")
 public class PostController {
+
+
+    @GetMapping
+    @RequestMapping ("/listaPosts")
+    public void getAll(){
+
+    }
+
+    @GetMapping
+    @RequestMapping("/singlePost")
+    public void getById(){
+
+    }
+
+    @PostMapping
+    @RequestMapping("/create")
+    public void createNewPost(){
+
+    }
+
+    @PutMapping
+    @RequestMapping("/editPost")
+    public void editPost(){
+
+    }
+    @DeleteMapping
+    @RequestMapping("/delete")
+    public void deletePost(){
+
+    }
 
 
 }
