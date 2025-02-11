9272bb845e18629c10ae7ddbe9db30c257ec4038
marty957 <fantuzzimartina@yahoo.it>
2025-02-11 16:50:59 +0100

    third commit

--- src/main/java/com/example/bloggingApp/Controller/AutoreController.java
+++ src/main/java/com/example/bloggingApp/Controller/AutoreController.java
@@ -1,8 +1,39 @@
 package com.example.bloggingApp.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/authors")
 public class AutoreController {
+
+    @GetMapping
+    @RequestMapping ("/listaAutori")
+    public void getAll(){
+
+    }
+
+    @GetMapping
+    @RequestMapping("/autore")
+    public void getById(){
+
+    }
+
+    @PostMapping
+    @RequestMapping("/createAutore")
+    public void createNewPost(){
+
+    }
+
+    @PutMapping
+    @RequestMapping("/editAutore")
+    public void editPost(){
+
+    }
+    @DeleteMapping
+    @RequestMapping("/delete")
+    public void deletePost(){
+
+    }
+
 }
